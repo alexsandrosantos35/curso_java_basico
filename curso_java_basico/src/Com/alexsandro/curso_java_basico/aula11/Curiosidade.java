b150b1a70701049f88f1f4b1da6edbb718a048a0
Alexsandro <Alexsandro@Alexsandro-PC>
2020-02-11 17:39:04 -0300

    Primeiro passo com o github

---
+++ curso_java_basico/src/Com/alexsandro/curso_java_basico/aula11/Curiosidade.java
@@ -0,0 +1,15 @@
+package Com.alexsandro.curso_java_basico.aula11;
+
+public class Curiosidade {
+
+	public static void main(String[] args) {
+		
+		int var1 = 2147483647;
+		
+		int var2 = 1;
+		
+		System.out.println(var1 + var2);
+
+	}
+
+}
---
+++ curso_java_basico/src/Erre01.java
@@ -0,0 +1,10 @@
+
+public class Erre01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
