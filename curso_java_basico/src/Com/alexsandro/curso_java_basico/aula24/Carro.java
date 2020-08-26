5b5f69b4061723ea2a0e323ad994e538c4e4e6c8
alexs <alexs@DESKTOP-43LPLIH>
2020-08-26 13:51:57 -0300

    Aula 24 classes carro e testecarro

---
+++ curso_java_basico/src/Com/alexsandro/curso_java_basico/aula24/Carro.java
@@ -0,0 +1,11 @@
+package Com.alexsandro.curso_java_basico.aula24;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+
+}
---
+++ curso_java_basico/src/Com/alexsandro/curso_java_basico/aula24/TesteCarro.java
@@ -0,0 +1,20 @@
+package Com.alexsandro.curso_java_basico.aula24;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducatto";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		//System.out.println(van);
+		System.out.println(van.marca);
+		System.out.println(van.modelo);
+
+	}
+
+}
